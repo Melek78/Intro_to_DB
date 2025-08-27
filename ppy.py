cf8096830df78b186524da727a22c75b2144bb99
Melek78 <abemelek@Abemelek.localdomain>
2025-08-27 19:56:29 +0300

    unwanted file pushed, removing ppy.py

--- ppy.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-alx_book_store = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "Melek@1234",
-    database = "alx_book_store",
-    ssl_disabled = True
-    )
-print(alx_book_store.get_server_info())
