a18547ea493565dfa3a8b3f363b735b06812a1a7
Melek78 <abemelek@Abemelek.localdomain>
2025-08-27 16:11:51 +0300

    sql first question

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,28 +1,28 @@
-create database alx_book_store;
-use alx_book_store
-CREATE TABLE Authors(
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store
+CREATE TABLE IF NOT EXISTS Authors(
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     authors_name VARCHAR(215) NOT NULL
 )
-CREATE TABLE Books(
+CREATE TABLE IF NOT EXISTS Books(
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130), NOT NULL
     FOREIGN KEY (author_id) REFERENCES Authors(author_id),
     price DOUBLE,
     publication_date DATE
 )
-CREATE TABLE Customers(
+CREATE TABLE IF NOT EXISTS Customers(
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 )
-CREATE TABLE Orders(
+CREATE TABLE IF NOT EXISTS Orders(
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 )
-CREATE TABLE Order_Details(
+CREATE TABLE IF NOT EXISTS Order_Details(
     orderdetailsid INT AUTO_INCREMENT PRIMARY KEY,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id),
