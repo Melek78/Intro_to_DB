a655fe231cad53a620f458dd84091d7af7825680
Melek78 <abemelek@Abemelek.localdomain>
2025-08-28 23:40:51 +0300

    task4.sql answer

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,14 @@
-USE alx_book_store;
-DESC Books;
+SELECT
+    COLUMN_NAME AS 'Column',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = alx_book_store()
+    AND TABLE_NAME = 'books'
+ORDER BY
+    ORDINAL_POSITION;
